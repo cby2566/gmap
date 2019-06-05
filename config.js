c75d9de7dd0d04d439702871ba372c729d1c0493
tony <373257720@qq.com>
2019-06-05 15:44:55 +0800

    new

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const ROLLBACK_TIME = 120000;
+const ROLLBACK_TIME_PARKED = 3600000;
 const LIST_REFRESH_INTERVAL = 500;
 const PARKED_REFRESH_INTERVAL = 5000;
 const DATA_FETCH_INTERVAL = 500;
@@ -8,5 +9,5 @@ const TRIP_END_TIMEOUT = 180000;
 const DEVICE_OFFLINE_TIMEOUT = 910000;
 const MAP_CENTERING_INTERVAL = 5000;
 const STOP_TIME_MIN = 30; /* seconds */
-const serverURL = "/gmap/"
-// const serverURL = window.require ? "http://localhost:8080/api/" : (window.location.href.indexOf("localhost") > 0 ? "/api/" : "/hub/api/");
+
+const serverURL = window.require ? "http://localhost:8080/api/" : (window.location.href.indexOf("localhost") > 0 ? "/api/" : "/hub/api/");
